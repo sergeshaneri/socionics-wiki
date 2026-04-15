6e900c99faee6c156cd2093fbc0776470c441cf2
Serge <shaneri.ser@gmail.com>
2026-04-16 00:57:10 +0300

    Rebuild and deploy with fixed GitHub Actions config

--- .astro/content-modules.mjs
+++
@@ -1,7 +0,0 @@
-
-export default new Map([
-["src/content/docs/concepts/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fconcepts%2Findex.mdx&astroContentModuleFlag=true")],
-["src/content/docs/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Findex.mdx&astroContentModuleFlag=true")],
-["src/content/docs/relations/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Frelations%2Findex.mdx&astroContentModuleFlag=true")],
-["src/content/docs/types/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Ftypes%2Findex.mdx&astroContentModuleFlag=true")]]);
-		
